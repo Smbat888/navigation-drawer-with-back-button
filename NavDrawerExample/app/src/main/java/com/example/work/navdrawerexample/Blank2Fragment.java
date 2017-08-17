8083bd0a20a0b902265575805f80db8225e6dcce
Smbat888 <smbatsargsyan1992@gmail.com>
2017-08-17 07:37:02 +0400

    Added whole project

---
+++ NavDrawerExample/app/src/main/java/com/example/work/navdrawerexample/Blank2Fragment.java
@@ -0,0 +1,46 @@
+package com.example.work.navdrawerexample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Blank2Fragment extends Fragment {
+
+
+    public Blank2Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_blank2, container, false);
+        Button m = (Button) view.findViewById(R.id.foo);
+        m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                foo();
+            }
+        });
+        return view;
+    }
+
+    public void foo() {
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new Blank3Fragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+}
